24cc75aa8e14391aa8a583693bc36bf5097ff0fb
ggabl1 <gabigcastro2@gmail.com>
2024-02-20 20:17:29 -0300

    adicionando lista1

---
+++ lista1/exercicio1/Program.cs
@@ -0,0 +1,15 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("digite as quatro notas: ");
+        double nota1 = double.Parse(Console.ReadLine());
+        double nota2 = double.Parse(Console.ReadLine());
+        double nota3 = double.Parse(Console.ReadLine());
+        double nota4 = double.Parse(Console.ReadLine());
+        double media = (nota1 + nota2 + nota3 + nota4) / 4;
+        Console.WriteLine("a media eh: " + media);
+
+
+    }
+}
